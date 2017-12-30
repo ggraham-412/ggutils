3063bf9af90a65d0da8ad94d8f6c04bd9ed6d7e9
Greg Graham <ggraham412@yahoo.com>
2017-12-30 16:18:03 -0600

    Added license

--- src/org/ggraham/ggutils/logging/ILogFormatter.java
+++ src/org/ggraham/ggutils/logging/ILogFormatter.java
@@ -1,5 +1,15 @@
 package org.ggraham.ggutils.logging;
 
+/*
+ * 
+ * Apache License 2.0 
+ * 
+ * Copyright (c) [2017] [Gregory Graham]
+ * 
+ * See LICENSE.txt for details.
+ * 
+ */
+
 public interface ILogFormatter {
 	
 	public String format(Object item);
